35662a68534912b081574134a899ee21ee0fbc69
mahesh garkhwal <mahesh.garkhwal@gmail.com>
2020-12-07 21:35:23 +0530

    database connection

--- fluffy-user-mangement/app.js
+++ fluffy-user-mangement/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connection = require("./database");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -26,6 +26,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+connection();
 
 // error handler
 app.use(function(err, req, res, next) {
